19d8118a0dfef1b523b575cfd113ae2a45f29f26
Mujeem8410 <muzeemsaifi88@gmail.com>
2025-10-25 22:02:51 +0530

    fix workflow

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express'); 
 const app = express();
+app.use(express.json());
 
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.post('/greeting', (req, res) => {
+  const name = req.body.name;
+  res.send(`Hello,: ${name} kya hall hai Bhai!`);
 });
 
 app.listen(port, () => {
