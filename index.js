ae9f167d9e87fc87f2d9cd4f04f98674659abd44
Mujeem8410 <muzeemsaifi88@gmail.com>
2025-10-26 12:42:26 +0530

    changes respins ein json form

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ app.use(cors());
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ message: 'Hello from the backend!' });
 });
 
 app.post('/greeting', (req, res) => {
   const name = req.body.name;
-  res.send(`Hello,: ${name} kya hall hai Bhai!`);
+  res.json({ message: `Hello,: ${name} kya hall hai Bhai!` });
 });
  
 app.get('/test', (req, res) => {
-  res.send('This is a test endpoint and make sure auto deploy by the CI and CD which the concept of DevOps...:)');
+  res.json({ message: 'This is a test endpoint and make sure auto deploy by the CI and CD which the concept of DevOps...:)' });
 });
 
 app.listen(port, () => {
